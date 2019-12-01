74a23e9e41f26b72be90b5e75c3b15064ac3b39d
Ryan Ngoy <brosmar@Ryans-MBP.hsd1.ca.comcast.net>
2019-11-30 20:46:14 -0800

    edit text for body

--- src/Body.js
+++ src/Body.js
@@ -7,7 +7,7 @@ class Body extends React.Component {
   render() {
   	return (
       <div className="Body">
-        <p> Below is a stateful square which will eventuall change as time passes! Currently if you click on it, there's a callback to change the internal color. </p>
+        <p> Below is a stateful square which will eventuall change as time passes! Currently if you click on it, there's a callback to change the internal color. Currently adding state changes via redux, but it's still a work in progress.</p>
         <ColorfulSquare />
       </div>
     );
