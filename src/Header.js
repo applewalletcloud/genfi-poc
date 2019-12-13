761e1098d277e347fe225bdc2dc757359aac7139
Ryan Ngoy <ni_xing@alumni.brown.edu>
2019-12-13 11:10:56 -0700

    adding react router dom modules

--- src/Header.js
+++ src/Header.js
@@ -2,18 +2,28 @@ import React from 'react';
 import './Header.css';
 import LoginButton from './LoginButton.js';
 import SignUpButton from './SignUpButton.js';
+import GoogleLogin from 'react-google-login';
 
 function Header() {
   return (
     <div className="Header">
-      <span> Welcome! This is my placeholder Header. It's just me playing around with flexbox. The buttons unfortunately do nothing at the moment! </span>
-      <span className="push">
-        <LoginButton text="Log in here!" />
-        <SignUpButton text="Sign up here!" />
-        <div class="g-signin2" data-onsuccess="onSignIn">
-          <button id="loginButton">Login with Google</button>
-        </div>
-      </span>
+      <div>
+	      <span> Welcome! This is my placeholder Header. It's just me playing around with flexbox. The buttons unfortunately do nothing at the moment! There's a google sign in button, which actually works! But it's not very significant at the moment. </span>
+	      <span className="push">
+	        <LoginButton text="Log in here!" />
+	        <SignUpButton text="Sign up here!" />
+	      </span>
+      </div>
+      <div>
+	      <GoogleLogin
+	          clientId="608003919007-o4s5bgjmms2hm378fdnatj15qhcsa15g.apps.googleusercontent.com"
+	          buttonText="LOGIN WITH GOOGLE"
+	          onSuccess={(response) => {
+	      console.log(response);}}
+	          onFailure={(response) => {
+	      console.log(response);}}
+	        />
+      </div>
     </div>
   );
 }
