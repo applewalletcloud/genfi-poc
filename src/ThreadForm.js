13fff9491bbd808ff450f74fac56c6448b73b64e
Ryan Ngoy <ni_xing@alumni.brown.edu>
2020-01-17 18:43:41 -0800

    made edits to form button

--- src/ThreadForm.js
+++ src/ThreadForm.js
@@ -2,13 +2,26 @@ import React from "react";
 import "./ThreadForm.css";
 import { Form } from 'react-bootstrap';
 
+function test(){
+	console.log("is historia dumb?")
+}
+
+function test2(e){
+	e.preventDefault();
+	console.log("success?")
+}
+
 function ThreadForm(props){
 	return (
 		<>
 			<div className="text-area-border">
+			<Form onSubmit={test2}>
 			  <Form.Control as="textarea" className="text-form" placeholder="Input your thoughts here!" rows="6" />
+			  
+			  <input className="text-area-button"type="submit" value="Submit" onSubmit={test2} />
+			  
+			</Form>
 			</div>
-			<input className="text-area-button"type="submit" value="Submit" />
 		</>
 	);
 }
